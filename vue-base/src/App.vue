dc32d33dd701d6572a9d1db9bba0ef17ca1c15ab
LemonYu997 <1445531395@qq.com>
2023-11-16 16:22:42 +0800

    vue-base

--- vue-base/src/App.vue
+++ vue-base/src/App.vue
@@ -1,47 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>模板语法/文本插值</h1>
+  <p>{{ msg }}</p>
+  <h2>也支持单一表达式，但是需要有返回值</h2>
+  <p>{{ number + 1 }}</p>
+  <p>{{ ok ? 'Yes' : 'No' }}</p>
+  <p>{{ message.split("").reverse().join("") }}</p>
+  <!-- 不能没有返回值 <p>{{ var a = 1 }}</p> -->
+  <!-- 不支持条件表达式，只支持三元运算符 <p>{{ if(ok) {return message} }}</p> -->
+  <h1>原始HTML</h1>
+  <p>{{ rawHtml }}</p>
+  <p v-html="rawHtml"></p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+export default {
+  data() {
+    return {
+      msg: "神奇的语法",
+      number: 10,
+      ok: true,
+      message: "大家好",
+      rawHtml: "<a href='https://www.baidu.com'>原始HTML</a>"
+    }
   }
 }
-</style>
+
+</script>
