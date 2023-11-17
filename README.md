d8d688a2f7abd8647116cb807e8449075da58dca
LemonYu997 <1445531395@qq.com>
2023-11-18 00:13:05 +0800

    vue应用

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Study-Vue3
 学习参考视频：https://www.bilibili.com/video/BV1Rs4y127j8
 
 Vue3官方文档：https://cn.vuejs.org/
+
+创建项目：
+```shell
+npm init vue@latest
+```
--- vue-base/src/main.js
+++ vue-base/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//app：vue的实例对象
+//每个项目有且只有一个Vue实例对象
+const app = createApp(App)
+
+//APP：根组件
+
+
+//挂载应用
+app.mount('#app')
