e4f1948ea30b01f09432c89ef1463eb69d746e20
LemonYu997 <1445531395@qq.com>
2023-11-17 18:22:37 +0800

    组件引用

---
+++ vue-components/src/components/MyComponest.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1 class="container">{{ message }}</h1>
+</template>
+  
+<script>
+
+export default {
+    data() {
+        return {
+            message: "组件基础组成"
+        }
+    }
+}
+
+</script>
+  
+<style>
+.container {
+    font-size: 30px;
+    color: red;
+}
+</style>
+  
+  
