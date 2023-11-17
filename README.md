648f541d38958d29e90a697bcf449d3ce2788444
LemonYu997 <1445531395@qq.com>
2023-11-17 23:43:06 +0800

    异步组件

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ Study-Vue3
 
 快速过一遍Vue3语法
 
-实践代码在 vue-demo/src/components 中
-
 学习参考视频：https://www.bilibili.com/video/BV1Rs4y127j8
 
 Vue3官方文档：https://cn.vuejs.org/
--- vue-components-life/src/App.vue
+++ vue-components-life/src/App.vue
@@ -6,15 +6,24 @@
   <keep-alive>
     <component :is="tabComponent"></component>
   </keep-alive>
+  <!-- 使用异步组件后，切换时才会加载组件B -->
   <button @click="changeHandle">切换组件</button>
 </template>
 
 <script>
 
+//异步组件加载依赖
+import {defineAsyncComponent} from 'vue'
+
 // import UseComponent from "./components/UserComponent.vue";
 
 import ComponentA from "./components/ComponentA.vue"
-import ComponentB from "./components/ComponentB.vue"
+// import ComponentB from "./components/ComponentB.vue"
+
+//异步组件，可以异步加载组件
+const ComponentB = defineAsyncComponent(() => 
+  import("./components/ComponentB.vue")
+)
 
 export default {
   data() {
