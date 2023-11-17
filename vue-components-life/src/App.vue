306aded10dcc6a5035bcad937debc28fc2669aa4
LemonYu997 <1445531395@qq.com>
2023-11-17 23:36:38 +0800

    组件切换

--- vue-components-life/src/App.vue
+++ vue-components-life/src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <UseComponent />
+  <!-- <UseComponent /> -->
+
+  <p>动态显示A组件或B组件</p>
+  <p>当切换组件时，被切换掉的组件会被卸载，可以通过 keep-alive 保活</p>
+  <keep-alive>
+    <component :is="tabComponent"></component>
+  </keep-alive>
+  <button @click="changeHandle">切换组件</button>
 </template>
 
 <script>
 
-import UseComponent from "./components/UserComponent.vue";
+// import UseComponent from "./components/UserComponent.vue";
+
+import ComponentA from "./components/ComponentA.vue"
+import ComponentB from "./components/ComponentB.vue"
 
 export default {
+  data() {
+    return {
+      //动态显示A组件或B组件
+      tabComponent: ComponentA
+    }
+  },
   components: {
-    UseComponent
+    // UseComponent,
+    ComponentA,
+    ComponentB
+  },
+  methods: {
+    changeHandle() {
+      //切换为A组件或B组件
+      this.tabComponent = this.tabComponent == "ComponentA" ? "ComponentB" : "ComponentA";
+    }
   }
 }
 
---
+++ vue-components-life/src/components/ComponentA.vue
@@ -0,0 +1,29 @@
+<template>
+    <h3>ComponentA</h3>
+    <p>{{ message }}</p>
+    <button @click="updateHandle">A组件——更新数据</button>
+    <br>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            message: "old value"
+        }
+    },
+    //keep-alive 保活的组件不会被卸载
+    beforeUnmount() {
+        console.log("组件A卸载之前");
+    },
+    unmounted() {
+        console.log("组件A卸载之后");
+    },
+    methods: {
+        updateHandle() {
+            this.message = "new value";
+        }
+    }
+}
+
+</script>
